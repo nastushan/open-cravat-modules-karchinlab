19ec6e3bb0ceda366ec3c2744093c50530756856
'rkiminsilico' <rkim@insilico.us.com>
2018-07-25 15:17:59 -0400

    hg38 writes 'Y' in coding column.

--- mappers/hg38/hg38.py
+++ mappers/hg38/hg38.py
@@ -415,9 +415,9 @@ class Mapper(cravat.BaseMapper):
         # Fill crx dict with information from most deleterious transcript
         crx_data['hugo'] = primary_hugo
         if primary_hugo != '':
-            crx_data['coding'] = 1
+            crx_data['coding'] = 'Y'
         else:
-            crx_data['coding'] = 0
+            crx_data['coding'] = ''
         crx_data['transcript'] = primary_transc
         crx_data['so'] = primary_so
         # Fill crx dict with all mappings
