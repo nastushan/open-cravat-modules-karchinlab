7068aa76a748ce7fb3bbf0af52467413cc3a48f2
Rick Kim <rkim@insilico.us.com>
2018-08-11 13:28:55 -0500

    finished update of yml developer

--- annotators/revel/revel.py
+++ annotators/revel/revel.py
@@ -5,15 +5,17 @@ import sqlite3
 import os
 
 class CravatAnnotator(BaseAnnotator):
-
     def annotate(self, input_data, secondary_data=None):
         chrom = input_data['chrom'].replace('chr','')
-        q = 'select score from revel where chrom="' + chrom + '" and pos=' + str(input_data['pos']) + ' and ref="' + input_data['ref_base'] + '" and alt="' + input_data['alt_base'] + '"'
-        self.cursor.execute(q)
-        r = self.cursor.fetchone()
-        if r:
-            out = {'score': r[0]}
-        else:
+        try:
+            q = 'select score from revel_' + chrom + ' where pos=' + str(input_data['pos']) + ' and ref="' + input_data['ref_base'] + '" and alt="' + input_data['alt_base'] + '"'
+            self.cursor.execute(q)
+            r = self.cursor.fetchone()
+            if r:
+                out = {'score': r[0]}
+            else:
+                out = None
+        except:
             out = None
         return out
         
