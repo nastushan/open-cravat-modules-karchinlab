b388ddca820a1c49ad010d20748bafce30d959c2
trakinsilico <trak@insilico.us.com>
2018-11-26 13:41:15 -0500

    md, yml, tests

--- webviewerwidgets/wgphastcons/wgphastcons.js
+++ webviewerwidgets/wgphastcons/wgphastcons.js
@@ -3,10 +3,10 @@ widgetGenerators['phastcons'] = {
 		'width': 280, 
 		'height': 80, 
 		'function': function (div, row, tabName) {
-			addInfoLine(div, row, 'Phast Cons 100 Way Vertebrate Score', 'phastcons__phastcons100_vert', tabName);
-			addInfoLine(div, row, 'Phast Cons 100 Way Vertebrate Ranked Score', 'phastcons__phastcons100_vert_r', tabName);
-			addInfoLine(div, row, 'Phast Cons 20 Way Mammalian Score', 'phastcons__phastcons20_mamm', tabName);
-			addInfoLine(div, row, 'Phast Cons 20 Way Mammalian Ranked Score', 'phastcons__phastcons20_mamm_r', tabName);
+			addInfoLine(div, row, 'phastCons 100 Way Vertebrate Score', 'phastcons__phastcons100_vert', tabName);
+			addInfoLine(div, row, 'phastCons 100 Way Vertebrate Ranked Score', 'phastcons__phastcons100_vert_r', tabName);
+			addInfoLine(div, row, 'phastCons 20 Way Mammalian Score', 'phastcons__phastcons20_mamm', tabName);
+			addInfoLine(div, row, 'phastCons 20 Way Mammalian Ranked Score', 'phastcons__phastcons20_mamm_r', tabName);
 		}
 	}
 }
--- webviewerwidgets/wgphylop/wgphylop.js
+++ webviewerwidgets/wgphylop/wgphylop.js
@@ -3,10 +3,10 @@ widgetGenerators['phylop'] = {
 		'width': 280, 
 		'height': 80, 
 		'function': function (div, row, tabName) {
-			addInfoLine(div, row, 'Phylo P 100 Way Vertebrate Score', 'phylop__phylop100_vert', tabName);
-			addInfoLine(div, row, 'Phylo P 100 Way Vertebrate Ranked Score', 'phylop__phylop100_vert_r', tabName);
-			addInfoLine(div, row, 'Phylo P 20 Way Mammalian Score', 'phylop__phylop20_mamm', tabName);
-			addInfoLine(div, row, 'Phylo P 20 Way Mammalian Ranked Score', 'phylop__phylop20_mamm_r', tabName);
+			addInfoLine(div, row, 'phyloP 100 Way Vertebrate Score', 'phylop__phylop100_vert', tabName);
+			addInfoLine(div, row, 'phyloP 100 Way Vertebrate Ranked Score', 'phylop__phylop100_vert_r', tabName);
+			addInfoLine(div, row, 'phyloP 20 Way Mammalian Score', 'phylop__phylop20_mamm', tabName);
+			addInfoLine(div, row, 'phyloP 20 Way Mammalian Ranked Score', 'phylop__phylop20_mamm_r', tabName);
 		}
 	}
 }
