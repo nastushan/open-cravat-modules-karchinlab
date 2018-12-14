6e74dada5ad4a08a220a27e09b2dfcfc6feab5e2
kmoad <kmoad@insilico.us.com>
2018-12-14 13:08:37 -0500

    grasp temp changes

--- annotators/grasp/grasp.py
+++ annotators/grasp/grasp.py
@@ -2,9 +2,11 @@ import sys
 import os
 import sqlite3
 from cravat import BaseAnnotator
-from cravat.util import get_ucsc_bins
 
 class CravatAnnotator (BaseAnnotator):
+
+    def setup(self):
+        self.query_template = 'select nhlbi, pmid, pvalue, phenotype from grasp where chrom=? and pos=? order by pvalue desc;'
 
     def annotate(self, input_data):
         out = {}
@@ -16,25 +18,16 @@ class CravatAnnotator (BaseAnnotator):
                'pmid': [],
                'phenotype': []}
         
-        bins = get_ucsc_bins(pos)
-        pos = str(pos)
-        for bin in bins:
-            query = 'select nhlbi, pmid, pvalue, phenotype ' +\
-                'from grasp ' +\
-                'where chrom="' + chrom +\
-                '" and binno=' + str(bin) +\
-                ' and pos=' + pos +\
-                ' order by pvalue desc;'
-            self.cursor.execute(query) 
-            results = self.cursor.fetchall()
-            if len(results) == 0:
-                continue
-            for result in results:
-                (nhlbi, pmid, pvalue, phenotype) = result
-                out['nhlbi'].append(nhlbi)
-                out['pmid'].append(pmid)
-                pvalue = '{:.4f}'.format(pvalue)
-                out['phenotype'].append(phenotype + '(' + str(pvalue) + ')')
+        self.cursor.execute(self.query_template, [chrom, pos]) 
+        results = self.cursor.fetchall()
+        if len(results) == 0:
+            continue
+        for result in results:
+            (nhlbi, pmid, pvalue, phenotype) = result
+            out['nhlbi'].append(nhlbi)
+            out['pmid'].append(pmid)
+            pvalue = '{:.4f}'.format(pvalue)
+            out['phenotype'].append(phenotype + '(' + str(pvalue) + ')')
         out['nhlbi'] = ','.join([str(v) for v in out['nhlbi']])
         out['pmid'] = ','.join([str(v) for v in out['pmid']])
         out['phenotype'] = ','.join([str(v) for v in out['phenotype']])
