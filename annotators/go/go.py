d9cf79b6c0ef849ee1c1471f61ae24802b48220f
trakinsilico <trak@insilico.us.com>
2018-12-17 17:17:34 -0500

    f

--- annotators/go/go.py
+++ annotators/go/go.py
@@ -9,32 +9,26 @@ class CravatAnnotator(BaseAnnotator):
     def annotate(self, input_data):
         out = {}
         hugo = input_data['hugo']
-        # if 'go_aspect' in self.conf:
-        #     aspect_filter = self.conf['go_aspect'].strip();
-        # else:
-        #     aspect_filter = 'F';
         
-        q = 'SELECT name, go_name.dname, go_name.id, aspect, refer, evidence FROM go_annotation JOIN go_name ON go_annotation.id=go_name.id WHERE hugo="%s";' \
+        q = 'SELECT dname, id, aspect, refer, evi FROM go_annotation JOIN go_name ON go_annotation.name=go_name.name WHERE go_name="%s";' \
             %(hugo)
         self.cursor.execute(q)
         result = self.cursor.fetchall()
         if result:
-            name_list = []
             id_list = []
             aspect_list = []
             ref_list = []
             evi_list = []
             for res in result:
-                name_list.append(res[0])
                 id_list.append(res[1])
                 aspect_list.append(res[2])
                 ref_list.append(res[3])
                 evi_list.append(res[4])
-            out['name'] = ';'.join(name_list)
+            out['name'] = res[0]
             out['id'] = ';'.join(id_list)
             out['aspect'] = ';'.join(aspect_list)
-            out['ref'] = ';'.join(ref_list)
-            out['evidence'] = ';'.join(evi_list)
+            out['refer'] = ';'.join(ref_list)
+            out['evi'] = ';'.join(evi_list)
         return out
     
 if __name__ == '__main__':
--- webviewerwidgets/wggo/wggo.js
+++ webviewerwidgets/wggo/wggo.js
@@ -3,23 +3,27 @@ widgetGenerators['go'] = {
 		'width': 450, 
 		'height': 200, 
 		'function': function (div, row, tabName) {
-			var go_name = infomgr.getRowValue(tabName, row, 'go__name');
-			var name_ls = go_name != null ? go_name.split(';') : [];
+			addInfoLine(div, row, 'Gene Name', 'go__name', tabName)
 			var goid = infomgr.getRowValue(tabName, row, 'go__id');
 			var idls = goid != null ? goid.split(';') : [];
 			var ontol = infomgr.getRowValue(tabName, row, 'go__aspect');
 			var ontolls = ontol != null ? ontol.split(';') : [];
+			var ref = infomgr.getRowValue(tabName, row, 'go__refer');
+			var refls = ontol != null ? ref.split(';') : [];
+			var evi = infomgr.getRowValue(tabName, row, 'go__evi');
+			var evils = ontol != null ? evi.split(';') : [];
 			var table = getWidgetTableFrame();
 			addEl(div, table);
-			var thead = getWidgetTableHead(['Term', 'ID', 'Ontology']);
+			var thead = getWidgetTableHead(['ID', 'Aspect', 'Reference', 'Evidence Code']);
 			addEl(table, thead);
 			var tbody = getEl('tbody');
 			addEl(table, tbody);
-			for (var i =0; i<name_ls.length;i++){
-				var nameitr = name_ls[i];
+			for (var i =0; i<idls.length;i++){
 				var iditr = idls[i];
 				var ontolitr = ontolls[i];
-				var tr = getWidgetTableTr([nameitr, iditr, ontolitr]);
+				var refitr = refls[i];
+				var eviitr = evils[i];
+				var tr = getWidgetTableTr([iditr, ontolitr, refitr, eviitr]);
 				addEl(tbody, tr);
 			}
 			addEl(div, addEl(table, tbody));
