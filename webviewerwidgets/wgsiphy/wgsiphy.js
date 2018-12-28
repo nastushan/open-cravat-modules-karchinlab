4bda1446e4880ca06b13c017e407746181699ee9
trakinsilico <trak@insilico.us.com>
2018-12-28 13:30:00 -0500

    siphy annotator 1st pass

---
+++ webviewerwidgets/wgsiphy/wgsiphy.js
@@ -0,0 +1,26 @@
+widgetGenerators['siphy'] = {
+	'variant': {
+		'width': 280, 
+		'height': 190, 
+		'function': function (div, row, tabName) {
+			addInfoLine(div, row, 'Score', 'siphy__logodds', tabName);
+			addGradientBarComponent(div, row, 'Rank Score', 'siphy__logodds_rank', tabName);
+			var pis = infomgr.getRowValue(tabName, row, 'siphy__pi');
+			var pils = pis != null ? pis.split(';') : [];
+			var table = getWidgetTableFrame();
+			addEl(div, table);
+			var thead = getWidgetTableHead(['Nucleobase', 'Stationary Distribution']);
+			addEl(table, thead);
+			var tbody = getEl('tbody');
+			addEl(table, tbody);
+			var bases = ['A', 'C', 'G', 'T']
+			for (var i =0; i<pils.length;i++){
+				var pi = pils[i]
+				var base = bases[i]
+				var tr = getWidgetTableTr([base, pi]);
+				addEl(tbody, tr);
+			}
+			addEl(div, addEl(table, tbody));
+		}
+	}
+}
