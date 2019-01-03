cbd2986e751d76926af1646a807ef734571737f3
trakinsilico <trak@insilico.us.com>
2019-01-03 17:02:28 -0500

    widget changes

--- webviewerwidgets/wginterpro/wginterpro.js
+++ webviewerwidgets/wginterpro/wginterpro.js
@@ -2,8 +2,9 @@ widgetGenerators['interpro'] = {
 	'variant': {
 		'width': 280, 
 		'height': 100, 
+		'word-break': 'normal',
 		'function': function (div, row, tabName) {
-			addInfoLine(div, row, 'Domain', 'interpro__domain', tabName);
+			addInfoLine(div, row, 'Domain', 'interpro__domain', tabName, 45);
 		}
 	}
 }
