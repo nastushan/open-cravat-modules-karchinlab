d356fe215ab7b9c1989e4700092deaa14dfab75f
trakinsilico <trak@insilico.us.com>
2019-01-03 12:37:03 -0500

    interpro first pass

---
+++ annotators/interpro/interpro.md
@@ -0,0 +1,3 @@
+# InterPro: 
+
+NOTE: Data provided by [dbNSFP](https://sites.google.com/site/jpopgen/dbNSFP) version 3.5a
---
+++ webviewerwidgets/wginterpro/wginterpro.js
@@ -0,0 +1,9 @@
+widgetGenerators['interpro'] = {
+	'variant': {
+		'width': 280, 
+		'height': 100, 
+		'function': function (div, row, tabName) {
+			addInfoLine(div, row, 'Domain', 'interpro__domain', tabName);
+		}
+	}
+}
