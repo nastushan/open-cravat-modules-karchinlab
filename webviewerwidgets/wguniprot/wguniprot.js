cf1a3ad703ce00b9cf402d938c22f45ccd0608dc
trakinsilico <trak@insilico.us.com>
2019-01-04 11:44:02 -0500

    uniprot

---
+++ webviewerwidgets/wguniprot/wguniprot.js
@@ -0,0 +1,22 @@
+widgetGenerators['uniprot'] = {
+	'gene': {
+		'width': 185, 
+		'height': 120, 
+		'function': function (div, row, tabName) {
+			var accs = infomgr.getRowValue(tabName, row, 'uniprot__acc');
+			var accsls = accs != null ? accs.split(';') : [];
+			var table = getWidgetTableFrame();
+			addEl(div, table);
+			var thead = getWidgetTableHead(['Acc Number', 'Hyperlink']);
+			addEl(table, thead);
+			var tbody = getEl('tbody');
+			addEl(table, tbody);
+			for (var j=0;j<accsls.length;j++){
+				var link = 'https://www.uniprot.org/uniprot/'+accsls[j];
+				var tr = getWidgetTableTr([accsls[j], link]);
+				addEl(tbody, tr);
+			addEl(div, addEl(table, tbody));
+			}
+		}
+	}
+}
