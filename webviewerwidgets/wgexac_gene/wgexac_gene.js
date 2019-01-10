9ec3809b8c4f75cd95a0d942b230dfcded3a0703
trakinsilico <trak@insilico.us.com>
2019-01-10 13:59:01 -0500

    exac visual changes

--- webviewerwidgets/wgexac_gene/wgexac_gene.js
+++ webviewerwidgets/wgexac_gene/wgexac_gene.js
@@ -10,15 +10,15 @@ widgetGenerators['exac_gene'] = {
 			else if (noise === 'Y'){
 				noise = 'Yes';
 			}
-			addInfoLine(div, row, 'Prob of LoF Intolerance (Homo & Hetero)', 'exac_gene__exac_pli', tabName);
-			addInfoLine(div, row, 'Prob of LoF Intolerance (Homo)', 'exac_gene__exac_prec', tabName);
-			addInfoLine(div, row, 'Prob of LoF Tolerance (Homo & Hetero)', 'exac_gene__exac_pnull', tabName);
-			addInfoLine(div, row, 'Prob of LoF Intolerance (Homo & Hetero) NonTCGA', 'exac_gene__exac_nontcga_pli', tabName);
-			addInfoLine(div, row, 'Prob of LoF Intolerance (Homo) NonTCGA', 'exac_gene__exac_nontcga_prec', tabName);
-			addInfoLine(div, row, 'Prob of LoF Tolerance (Homo & Hetero) NonTCGA', 'exac_gene__exac_nontcga_pnull', tabName);
-			addInfoLine(div, row, 'Prob of LoF Intolerance (Homo & Hetero) Nonpsych', 'exac_gene__exac_nonpsych_pli', tabName);
-			addInfoLine(div, row, 'Prob of LoF Intolerance (Homo) Nonpsych', 'exac_gene__exac_nonpsych_prec', tabName);
-			addInfoLine(div, row, 'Prob of LoF Tolerance (Homo & Hetero) Nonpsych', 'exac_gene__exac_nonpsych_pnull', tabName);
+			addInfoLine(div, row, 'Prob of LoF Intolerance (Hom & Het)', 'exac_gene__exac_pli', tabName);
+			addInfoLine(div, row, 'Prob of LoF Intolerance (Hom)', 'exac_gene__exac_prec', tabName);
+			addInfoLine(div, row, 'Prob of LoF Tolerance (Hom & Het)', 'exac_gene__exac_pnull', tabName);
+			addInfoLine(div, row, 'Prob of LoF Intolerance (Hom & Het) NonTCGA', 'exac_gene__exac_nontcga_pli', tabName);
+			addInfoLine(div, row, 'Prob of LoF Intolerance (Hom) NonTCGA', 'exac_gene__exac_nontcga_prec', tabName);
+			addInfoLine(div, row, 'Prob of LoF Tolerance (Hom & Het) NonTCGA', 'exac_gene__exac_nontcga_pnull', tabName);
+			addInfoLine(div, row, 'Prob of LoF Intolerance (Hom & Het) Nonpsych', 'exac_gene__exac_nonpsych_pli', tabName);
+			addInfoLine(div, row, 'Prob of LoF Intolerance (Hom) Nonpsych', 'exac_gene__exac_nonpsych_prec', tabName);
+			addInfoLine(div, row, 'Prob of LoF Tolerance (Hom & Het) Nonpsych', 'exac_gene__exac_nonpsych_pnull', tabName);
 			addInfoLine(div, row, 'Winsorised Deletion Intolerance Z-Score', 'exac_gene__exac_del_score', tabName);
 			addInfoLine(div, row, 'Winsorised Duplication Intolerance Z-Score', 'exac_gene__exac_dup_score', tabName);
 			addInfoLine(div, row, 'Winsorised CNV Intolerance Z-Score', 'exac_gene__exac_cnv_score', tabName);
