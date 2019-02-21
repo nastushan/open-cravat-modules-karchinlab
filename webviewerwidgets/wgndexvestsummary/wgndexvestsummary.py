cf6160cc968ba300c1d899981c2fa2940c0c7765
Rick Kim <rkim@insilico.us.com>
2019-02-21 16:28:23 -0500

    removed print from wgndexvestsummary

--- webviewerwidgets/wgndexvestsummary/wgndexvestsummary.py
+++ webviewerwidgets/wgndexvestsummary/wgndexvestsummary.py
@@ -22,9 +22,7 @@ def run_query (hugos):
 def get_data (queries):
     hugos = queries['hugos']
     hugos = json.loads(hugos)
-    print('hugos=', hugos)
     response = {'data': run_query(hugos)}
-    print('response=', response)
     return response
 
 if __name__ ==  '__main__':
