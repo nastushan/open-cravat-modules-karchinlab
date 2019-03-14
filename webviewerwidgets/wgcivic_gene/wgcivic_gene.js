383217b206cbb117ed360d348688421595df9aa9
kmoad <kmoad@mail.insilico.us.com>
2019-03-14 17:21:50 -0400

    update civic and civic_gene to use new link format

--- webviewerwidgets/wgcivic_gene/wgcivic_gene.js
+++ webviewerwidgets/wgcivic_gene/wgcivic_gene.js
@@ -3,13 +3,6 @@ widgetGenerators['civic_gene'] = {
 		'width': 280, 
 		'height': 80, 
 		'function': function (div, row, tabName) {
-			var link = infomgr.getRowValue(tabName, row, 'civic_gene__link');
-			if (link == null) {
-				link = 'None';
-				addInfoLineText(div, 'Link', link);
-			} else {
-				addInfoLineLink(div, 'Link', link, link, -1);
-			}
 			addInfoLine(div, row, 'Description', 'civic_gene__description', tabName, 70);
 		}
 	}
