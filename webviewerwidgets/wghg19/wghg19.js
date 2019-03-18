d9cc6d7b2d9b42f3a37849abad150b768b1f6a7a
Rick Kim <rkim@insilico.us.com>
2019-03-18 14:24:30 -0400

    hidden wghg19 wgvcfinfo

--- webviewerwidgets/wghg19/wghg19.js
+++ webviewerwidgets/wghg19/wghg19.js
@@ -2,7 +2,6 @@ widgetGenerators['hg19'] = {
 	'variant': {
 		'width': 280, 
 		'height': 80, 
-        'default_hidden': true,
 		'function': function (div, row, tabName) {
 			addInfoLine(div, row, 'Chromosome', 'hg19__chrom', tabName);
 			addInfoLine(div, row, 'Position', 'hg19__pos', tabName);
