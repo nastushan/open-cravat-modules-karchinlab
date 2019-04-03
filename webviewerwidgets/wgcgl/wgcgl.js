67b34c575bc15b8b5bc24662dfa447f61f15ed0c
Kyle Moad <kmoad@insilico.us.com>
2019-04-03 18:49:54 -0400

    change cgl title

--- webviewerwidgets/wgcgl/wgcgl.js
+++ webviewerwidgets/wgcgl/wgcgl.js
@@ -1,7 +1,7 @@
 widgetGenerators['cgl'] = {
 	'gene': {
-		'width': 280, 
-		'height': 180, 
+		'width': 180, 
+		'height': 80, 
         'default_hidden': true,
 		'function': function (div, row, tabName) {
 			addInfoLine(div, row, 'Class', 'cgl__class', tabName);
