e02be9f74021e85f484e247adcec5bb257f078db
Rick Kim <rkim@insilico.us.com>
2019-04-17 16:15:14 +0900

    async wgnote

--- webviewerwidgets/wgnote/wgnote.py
+++ webviewerwidgets/wgnote/wgnote.py
@@ -1,26 +1,28 @@
-import sqlite3
+import aiosqlite3
 
-def get_data (queries):
+async def get_data (queries):
     ret = {}
     try:
         dbpath = queries['dbpath']
         tab = queries['tab']
         rowkey = queries['rowkey']
         note = queries['note'].replace('"', "'")
-        conn = sqlite3.connect(dbpath)
-        c = conn.cursor()
+        conn = await aiosqlite3.connect(dbpath)
+        c = await conn.cursor()
         if tab == 'variant':
             q = 'update {} set base__note="{}" where base__uid={}'.format(
                 tab, note, rowkey)
-            c.execute(q)
+            await c.execute(q)
         elif tab == 'gene':
             q = 'update {} set base__note="{}" where base__hugo="{}"'.format(
                 tab, note, rowkey)
-            c.execute(q)
-        conn.commit()
+            await c.execute(q)
+        await conn.commit()
         ret['status'] = 'success'
     except:
         import traceback
         traceback.print_exc()
         ret['status'] = 'fail'
+    await c.close()
+    await conn.close()
     return ret
