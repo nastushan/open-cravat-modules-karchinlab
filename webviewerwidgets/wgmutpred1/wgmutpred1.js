517b9ba9acd85b6a7dd33a7d3dfce030f40978b9
Kyle Moad <kmoad@insilico.us.com>
2019-07-15 12:25:10 -0400

    remove extra vest widget from wgmutpred1

--- webviewerwidgets/wgmutpred1/wgmutpred1.js
+++ webviewerwidgets/wgmutpred1/wgmutpred1.js
@@ -37,14 +37,5 @@ widgetGenerators['mutpred1'] = {
 				addEl(div, addEl(table, tbody));
 			}
 		}
-	},
-	'gene': {
-		'width': 280, 
-		'height': 80, 
-		'function': function (div, row, tabName) {
-			addInfoLine(div, row, 'Max score', 'vest__max_score', tabName);
-			addInfoLine(div, row, 'Mean score', 'vest__mean_score', tabName);
-			addInfoLine(div, row, 'Gene p-value', 'vest__gene_pval', tabName);
-		}
 	}
 }
