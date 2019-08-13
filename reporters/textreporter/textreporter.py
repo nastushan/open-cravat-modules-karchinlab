823c4131c1f295bfef5a09775ce299fb895fbaae
Rick Kim <rkim@insilico.us.com>
2019-08-13 11:55:31 -0400

    sync textreporter

--- reporters/textreporter/textreporter.py
+++ reporters/textreporter/textreporter.py
@@ -57,7 +57,6 @@ class Reporter(CravatReport):
     def write_preface_line (self, line):
         self.wf.write('#' + line + '\n')
 
-    '''
     def substitute_val (self, level, row):
         if level in self.column_subs:
             for i in self.column_subs[level]:
@@ -66,7 +65,6 @@ class Reporter(CravatReport):
                     for target in sub:
                         row[i] = re.sub('\\b' + target + '\\b', sub[target], row[i])
         return row
-    '''
 
 def main ():
     reporter = Reporter(sys.argv)
