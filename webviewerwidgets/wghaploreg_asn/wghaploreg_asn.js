fa7987973fc38d8bd8df421d7bfc9608745288ce
kmoad <kmoad@mail.insilico.us.com>
2019-10-22 09:40:33 -0400

    haploreg widgets

---
+++ webviewerwidgets/wghaploreg_asn/wghaploreg_asn.js
@@ -0,0 +1,26 @@
+widgetGenerators['haploreg_asn'] = {
+	'variant': {
+		'width': 380, 
+		'height': 200, 
+		'function': function (div, row, tabName) {
+			if (infomgr.getColumnNo('variant', 'base__hugo')) {
+				var snps = infomgr.getRowValue(tabName, row, 'haploreg_asn__snps').split(',');
+				var r2s = infomgr.getRowValue(tabName, row, 'haploreg_asn__r2s').split(',').map(parseFloat);
+				var dprimes = infomgr.getRowValue(tabName, row, 'haploreg_asn__dprimes').split(',').map(parseFloat);
+				if (snps && r2s && dprimes) {
+					var table = getWidgetTableFrame();
+                    table.style.tableLayout = 'auto';
+					table.style.width = '100%';
+					var thead = getWidgetTableHead(['rsID', 'R\u00b2', 'D\''], ['50%', '25%', '25%']);
+					addEl(table, thead);
+					var tbody = getEl('tbody');
+					for (var i=0; i<snps.length; i++) {
+						var tr = getWidgetTableTr([snps[i], r2s[i], dprimes[i]]);
+						addEl(tbody, tr);
+					}
+					addEl(div, addEl(table, tbody));
+				}
+			}
+		}
+	}
+}
