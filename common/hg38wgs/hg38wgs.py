e429e8f54d8ff40d5a6c79fd4b7645564720b47e
Rick Kim <rkim@insilico.us.com>
2020-02-13 14:56:13 -0500

    hg38wgs with rev n

---
+++ common/hg38wgs/hg38wgs.py
@@ -0,0 +1,35 @@
+import twobitreader
+import os
+from cravat import BaseCommonModule
+
+class CravatCommonModule (BaseCommonModule):
+    def setup (self):
+        self.wgs_reader = twobitreader.TwoBitFile(os.path.join(os.path.dirname(__file__), 'data', 'hg38.2bit'))
+        self.revbases = {'A':'T', 'T':'A', 'G':'C', 'C':'G', 'N':'N', 'a':'t', 't':'a', 'g':'c', 'c':'g', 'n':'n'}
+
+    def __getitem__ (self, chrom):
+        return self.wgs_reader[chrom]
+
+    def get_bases (self, chrom, start, end=None, strand=None):
+        if end is None:
+            end = start
+        if chrom not in self.wgs_reader:
+            return None
+        if strand is None or strand == '+':
+            if start <= end:
+                return self.wgs_reader[chrom][start - 1:end]
+            else:
+                bases = ''
+                for pos in range(start - 1, end - 2, -1):
+                    bases += self.wgs_reader[chrom][pos]
+                return bases
+        elif strand == '-':
+            if start <= end:
+                bases = ''
+                for pos in range(end - 1, start - 2, -1):
+                    bases += self.wgs_reader[chrom][pos]
+                return ''.join([self.revbases[b] for b in bases])
+            else:
+                return ''.join([self.revbases[b] for b in self.wgs_reader[chrom][end - 1:start]])
+        else:
+            return base
