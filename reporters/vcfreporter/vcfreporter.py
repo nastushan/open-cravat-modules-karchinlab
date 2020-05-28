6a9f180c8efe8f4bf3baae6803918b39357452b9
Rick Kim <rkim@insilico.us.com>
2020-05-28 10:32:46 -0400

    removed substitute_val from vcfreporter

--- reporters/vcfreporter/vcfreporter.py
+++ reporters/vcfreporter/vcfreporter.py
@@ -309,15 +309,6 @@ class Reporter(CravatReport):
             return
         self.wf.write('#' + line + '\n')
 
-    def substitute_val (self, level, row):
-        if level in self.column_subs:
-            for i in self.column_subs[level]:
-                if row[i] is not None:
-                    sub = self.column_subs[level][i]
-                    for target in sub:
-                        row[i] = re.sub('\\b' + target + '\\b', sub[target], row[i])
-        return row
-
 def main ():
     reporter = Reporter(sys.argv)
     reporter.run()
